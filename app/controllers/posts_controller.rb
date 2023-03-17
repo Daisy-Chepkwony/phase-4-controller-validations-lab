e27ede27438d99f625dd1f6799f49a00285a25f2
+ <+>
2023-03-17 16:09:36 +0300

    commit tree 2e7217e999ed82d9ab5cfe1bfc3ca55eac688000

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def show
     post = Post.find(params[:id])
@@ -9,7 +11,7 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
@@ -19,5 +21,76 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:category, :content, :title)
   end
+  def render_not_found_response
+        render json: { error: "  Post not found" }, status: :not_found
+  end
+    def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
 
 end
+
+
+
+
+
+
+
+# class BirdsController < ApplicationController
+#   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+#   # added rescue_from
+#   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+#   # rest of controller actions...
+
+#   # GET /birds
+#   def index
+#     birds = Bird.all
+#     render json: birds
+#   end
+
+#   # POST /birds
+#   def create
+#     bird = Bird.create!(bird_params)
+#     render json: bird, status: :created
+#   end
+
+#   # GET /birds/:id
+#   def show
+#     bird = find_bird
+#     render json: bird
+#   end
+
+#   # PATCH /birds/:id
+#   def update
+#     bird = find_bird
+#     bird.update!(bird_params)
+#     render json: bird
+#   end
+
+#   # DELETE /birds/:id
+#   def destroy
+#     bird = find_bird
+#     bird.destroy
+#     head :no_content
+#   end
+
+#   private
+
+#   def find_bird
+#     Bird.find(params[:id])
+#   end
+
+#   def bird_params
+#     params.permit(:name, :species, :likes)
+#   end
+
+#   def render_not_found_response
+#     render json: { error: "Bird not found" }, status: :not_found
+#   end
+
+#   def render_unprocessable_entity_response(invalid)
+#     render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+#   end
+
+# end
